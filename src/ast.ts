a474edec4409deb84a97ed902f6a34d1e003b89c
William Chen <william@terran.one>
2022-12-16 22:47:11 -0800

    add eval to test

--- src/ast.ts
+++ src/ast.ts
@@ -1,140 +1,208 @@
+import { Tuple } from "runtypes";
+
 export class Node {
-	constructor(public _parent: Node | null = null) {
-	}
+  constructor(public _parent: Node | null = null) {
+  }
 
-	setParentForChildren() {
-		for (let key in this) {
-			if (key !== '_parent' && this[key] instanceof Node) {
-				// @ts-ignore
-				this[key]._parent = this;
-			}
+  setParentForChildren() {
+    for (let key in this) {
+      if (key !== '_parent' && this[key] instanceof Node) {
+        // @ts-ignore
+        this[key]._parent = this;
+      }
 
-			if (key !== '_parent' && this[key] instanceof List) {
-				console.log(this[key]);
-				// @ts-ignore
-				this[key].forEach((item: Node) => item._parent = this);
-			}
-		}
-	}
+      if (key !== '_parent' && this[key] instanceof List) {
+        // @ts-ignore
+        this[key].forEach((item: Node) => item._parent = this);
+      }
+    }
+  }
 
-	walkDescendantsDFS(): Node[] {
-		let nodes: Node[] = [];
-		for (let key in this) {
-			if (key !== '_parent' && this[key] instanceof Node) {
-				// @ts-ignore
-				nodes.push(this[key]);
-				// @ts-ignore
-				nodes = nodes.concat(this[key].walkDescendantsDFS());
-			}
-		}
-		return nodes;
-	}
+  walkDescendantsDFS(): Node[] {
+    let nodes: Node[] = [];
+    for (let key in this) {
+      if (key !== '_parent' && this[key] instanceof Node) {
+        // @ts-ignore
+        nodes.push(this[key]);
+        // @ts-ignore
+        nodes = nodes.concat(this[key].walkDescendantsDFS());
+      }
+    }
+    return nodes;
+  }
 
-	walkDescendantsBFS(): Node[] {
-		let nodes: Node[] = [];
-		let queue: Node[] = [this];
-		while (queue.length > 0) {
-			let node = queue.shift()!;
-			for (let key in node) {
-				// @ts-ignore
-				if (key !== '_parent' && node[key] instanceof Node) {
-					// @ts-ignore
-					nodes.push(node[key]);
-					// @ts-ignore
-					queue.push(node[key]);
-				}
-			}
-		}
-		return nodes;
-	}
+  walkDescendantsBFS(): Node[] {
+    let nodes: Node[] = [];
+    let queue: Node[] = [this];
+    while (queue.length > 0) {
+      let node = queue.shift()!;
+      for (let key in node) {
+        // @ts-ignore
+        if (key !== '_parent' && node[key] instanceof Node) {
+          // @ts-ignore
+          nodes.push(node[key]);
+          // @ts-ignore
+          queue.push(node[key]);
+        }
+      }
+    }
+    return nodes;
+  }
 
-	descendantsWhere(predicate: (node: Node) => boolean): Node[] {
-		return this.walkDescendantsDFS().filter(predicate);
-	}
+  descendantsWhere(predicate: (node: Node) => boolean): Node[] {
+    return this.walkDescendantsDFS().filter(predicate);
+  }
 
-	nearestAncestorWhere(predicate: (node: Node) => boolean): Node | null {
-		let node: Node | null = this;
-		while (node !== null && !predicate(node)) {
-			node = node._parent;
-		}
-		return node;
-	}
+  nearestAncestorWhere(predicate: (node: Node) => boolean): Node | null {
+    let node: Node | null = this;
+    while (node !== null && !predicate(node)) {
+      node = node._parent;
+    }
+    return node;
+  }
 
-	toData(): any {
-		let data: any = {};
-		for (let key in this) {
-			if (key !== '_parent' && this[key] instanceof Node) {
-				// @ts-ignore
-				data[key] = this[key].toData();
-			} else if (key !== '_parent' && this[key] instanceof List) {
-				// @ts-ignore
-				data[key] = this[key].toData();
-			} else if (key !== '_parent') {
-				// @ts-ignore
-				data[key] = this[key];
-			}
-		}
-		return {[this.constructor.name]: data};
-	}
+  toData(): any {
+    let data: any = {};
+    for (let key in this) {
+      if (key !== '_parent' && this[key] instanceof Node) {
+        // @ts-ignore
+        data[key] = this[key].toData();
+      } else if (key !== '_parent' && this[key] instanceof List) {
+        // @ts-ignore
+        data[key] = this[key].toData();
+      } else if (key !== '_parent') {
+        // @ts-ignore
+        data[key] = this[key];
+      }
+    }
+    return { [this.constructor.name]: data };
+  }
+
+  /**
+  * Checks if this node is subtype of (extends) the given type. 
+  * @param {Node} node to compare
+    */
+  isSubtypeOf(other: Node): boolean {
+    if (this.equals(other)) {
+      return true;
+    } else if (other.equals(Primitive.Any())) {
+      return true;
+    } else if (other.equals(Primitive.Never())) {
+      return false; // precedence -- 'never' will still extend 'never'
+    } else {
+      return false;
+    }
+  }
+
+  equals(other: Node): boolean {
+    if (other instanceof Node) {
+      return this.toData() === other.toData();
+    }
+    return false;
+  }
 }
 
+export class List<T extends Node> extends Node implements Iterable<T> {
+  constructor(public items: T[]) {
+    super();
+    this.setParentForChildren();
+  }
 
-export class List<T extends Node> extends Node {
-	constructor(public items: T[]) {
-		super();
-		this.setParentForChildren();
-	}
+  [Symbol.iterator](): Iterator<T> {
+    return this.items[Symbol.iterator]();
+  }
 
-	toData(): any {
-		return {
-			List: {
-				items: this.items.map(item => item.toData())
-			}
-		}
-	}
+  toData(): any {
+    return {
+      List: {
+        items: this.items.map(item => item.toData())
+      }
+    }
+  }
 
-	walkDescendantsDFS(): Node[] {
-		let nodes: Node[] = [];
-		for (let item of this.items) {
-			nodes.push(item);
-			nodes = nodes.concat(item.walkDescendantsDFS());
-		}
-		return nodes;
-	}
+  walkDescendantsDFS(): Node[] {
+    let nodes: Node[] = [];
+    for (let item of this.items) {
+      nodes.push(item);
+      nodes = nodes.concat(item.walkDescendantsDFS());
+    }
+    return nodes;
+  }
 
-	walkDescendantsBFS(): Node[] {
-		let nodes: Node[] = [];
-		let queue: Node[] = this.items;
-		while (queue.length > 0) {
-			let node = queue.shift()!;
-			for (let key in node) {
-				// @ts-ignore
-				if (key !== '_parent' && node[key] instanceof Node) {
-					// @ts-ignore
-					nodes.push(node[key]);
-					// @ts-ignore
-					queue.push(node[key]);
-				}
-			}
-		}
-		return nodes;
-	}
+  walkDescendantsBFS(): Node[] {
+    let nodes: Node[] = [];
+    let queue: Node[] = this.items;
+    while (queue.length > 0) {
+      let node = queue.shift()!;
+      for (let key in node) {
+        // @ts-ignore
+        if (key !== '_parent' && node[key] instanceof Node) {
+          // @ts-ignore
+          nodes.push(node[key]);
+          // @ts-ignore
+          queue.push(node[key]);
+        }
+      }
+    }
+    return nodes;
+  }
 
-	static empty<T extends Node>(): List<T> {
-		return new List<T>([]);
-	}
+  static empty<T extends Node>(): List<T> {
+    return new List<T>([]);
+  }
 
-	static of<T extends Node>(items: T[]): List<T> {
-		return new List<T>(items);
-	}
+  static of<T extends Node>(items: T[]): List<T> {
+    return new List<T>(items);
+  }
 
-	map<U>(fn: (item: T) => U): U[] {
-		return this.items.map(fn);
-	}
+  map<U>(fn: (element: T, index: number, array: T[]) => U): U[] {
+    return this.items.map<U>(fn);
+  }
 
-	forEach(fn: (item: T) => void): void {
-		this.items.forEach(fn);
-	}
+  forEach(...params: Parameters<Array<T>['forEach']>): void {
+    this.items.forEach(...params);
+  }
+
+  get length(): number {
+    return this.items.length;
+  }
+
+  first(): T | undefined {
+    if (this.length >= 1) {
+      return this.items[0];
+    }
+    return undefined;
+  }
+
+  last(): T | undefined {
+    if (this.length >= 1) {
+      return this.items[this.length - 1];
+    }
+    return undefined;
+  }
+
+  rest(): T[] {
+    if (this.length >= 1) {
+      return this.items.slice(1);
+    }
+    return [];
+  }
+
+  at(index: number): T {
+    return this.items[index];
+  }
+
+  every(predicate: (item: T) => boolean): boolean {
+    return this.items.every(predicate);
+  }
+
+  some(predicate: (item: T) => boolean): boolean {
+    return this.items.some(predicate);
+  }
+
+  slice(start?: number, end?: number): T[] {
+    return this.items.slice(start, end);
+  }
 
 }
 
@@ -143,101 +211,263 @@ export type Atom = Primitive | Literal | Identifier;
 export type Literal = BooleanLiteral | StringLiteral | NumberLiteral | TupleLiteral | ObjectLiteral
 
 export class CompilationUnit extends Node {
-	constructor(public sexprs: List<SExpr>) {
-		super();
-		this.setParentForChildren();
-	}
+  constructor(public sexprs: List<SExpr>) {
+    super();
+    this.setParentForChildren();
+  }
 }
 
 export enum PrimitiveType {
-	Number = 'number',
-	String = 'string',
-	Boolean = 'boolean',
-	Never = 'never',
-	Null = 'null',
-	Undefined = 'undefined',
-	Unknown = 'unknown',
-	Object = 'object',
-	Any = 'any',
+  Number = 'number',
+  String = 'string',
+  Boolean = 'boolean',
+  Never = 'never',
+  Null = 'null',
+  Undefined = 'undefined',
+  Unknown = 'unknown',
+  Object = 'object',
+  Any = 'any',
 }
 
 export class ArrayOf extends Node {
-	constructor(public ty: SExpr) {
-		super();
-		this.setParentForChildren();
-	}
+  constructor(public ty: SExpr) {
+    super();
+    this.setParentForChildren();
+  }
 }
 
 export class IndexOf extends Node {
-	constructor(public ty: SExpr, public index: SExpr) {
-		super();
-		this.setParentForChildren();
-	}
+  constructor(public ty: SExpr, public index: SExpr) {
+    super();
+    this.setParentForChildren();
+  }
 }
 
 export class Call extends Node {
-	constructor(public ty: SExpr, public args: List<SExpr>) {
-		super();
-		this.setParentForChildren();
-	}
+  constructor(public ty: SExpr, public args: List<SExpr>) {
+    super();
+    this.setParentForChildren();
+  }
 }
 
 export class Spread extends Node {
-	constructor(public ty: SExpr) {
-		super();
-		this.setParentForChildren();
-	}
+  constructor(public ty: SExpr) {
+    super();
+    this.setParentForChildren();
+  }
 }
 
 export class Primitive extends Node {
-	constructor(public ty: PrimitiveType) {
-		super();
-		this.setParentForChildren();
-	}
+  constructor(public ty: PrimitiveType) {
+    super();
+    this.setParentForChildren();
+  }
+
+  static Never(): Primitive {
+    return new Primitive(PrimitiveType.Never);
+  }
+
+  static Any(): Primitive {
+    return new Primitive(PrimitiveType.Any);
+  }
+
+  static Null(): Primitive {
+    return new Primitive(PrimitiveType.Null);
+  }
+
+  static Undefined(): Primitive {
+    return new Primitive(PrimitiveType.Undefined);
+  }
+
+  static Object(): Primitive {
+    return new Primitive(PrimitiveType.Object);
+  }
+
+  static String(): Primitive {
+    return new Primitive(PrimitiveType.String);
+  }
+
+  static Number(): Primitive {
+    return new Primitive(PrimitiveType.Number);
+  }
+
+  static Boolean(): Primitive {
+    return new Primitive(PrimitiveType.Boolean);
+  }
+
+  static Unknown(): Primitive {
+    return new Primitive(PrimitiveType.Unknown);
+  }
+
 }
 
 export class Identifier extends Node {
-	constructor(public text: string) {
-		super();
-		this.setParentForChildren();
-	}
+  constructor(public text: string) {
+    super();
+    this.setParentForChildren();
+  }
 }
 
 export class ObjectLiteral extends Node {
-	constructor(public props: List<Prop>) {
-		super();
-		this.setParentForChildren();
-	}
+  constructor(public props: List<Prop>) {
+    super();
+    this.setParentForChildren();
+  }
 }
 
 export class Prop extends Node {
-	constructor(public key: Identifier, public value: SExpr) {
-		super();
-		this.setParentForChildren();
-	}
+  constructor(public key: Identifier, public value: SExpr) {
+    super();
+    this.setParentForChildren();
+  }
 }
 
 export class TupleLiteral extends Node {
-	constructor(public items: List<SExpr>) {
-		super();
-		this.setParentForChildren();
-	}
-}
+  constructor(public items: List<SExpr> = List.empty()) {
+    super();
+    this.setParentForChildren();
+  }
 
-export class StringLiteral extends Node {
-	constructor(public value: string) {
-		super();
-	}
-}
+  first(): TupleLiteral {
+    let first = this.items.first();
+    if (first) {
+      return new TupleLiteral(List.of([first]));
+    }
+    return new TupleLiteral();
+  }
 
-export class BooleanLiteral extends Node {
-	constructor(public value: boolean) {
-		super();
-	}
-}
+  rest(): TupleLiteral {
+    let rest = this.items.rest();
+    return new TupleLiteral(List.of(rest));
+  }
 
-export class NumberLiteral extends Node {
-	constructor(public value: number) {
-		super();
-	}
-}
+  last(): TupleLiteral {
+    let last = this.items.last();
+    if (last) {
+      return new TupleLiteral(List.of([last]));
+    }
+    return new TupleLiteral();
+  }
+
+  // assumes simplified
+  isSubtypeOf(other: Node): boolean {
+    if (other instanceof TupleLiteral) {
+      // base case: [] extends []
+      if (this.items.length === 0 && this.items.length === other.items.length) {
+        return true;
+      }
+
+      // base case: [a] extends [a]
+      if (this.items.length === 1 && this.items.length === other.items.length) {
+        return this.items.at(0)!.isSubtypeOf(other.items.at(0)!);
+      }
+
+      // setup
+      let first = this.items.first();
+      let rest = this.items.rest();
+      let last = this.items.last();
+
+      let otherFirst = other.items.first();
+      let otherRest = other.items.rest();
+      let otherLast = other.items.last();
+
+      // check spreads
+      if (first && first instanceof Spread) {
+        // remove common first
+
+        if (otherFirst && otherFirst instanceof Spread) {
+          // [...number[], string, boolean] extends X = [...number[], string, boolean]
+          // -> rest(this) extends rest(other)
+          if (!this.rest().isSubtypeOf(other.rest())) {
+            return false;
+          }
+          // -> first(this) extends first(other)
+          if (!this.first().isSubtypeOf(other.first())) {
+            return false;
+          }
+        } else if (otherLast && otherLast instanceof Spread) {
+          // [...number[], boolean, string, string] extends X = [number, number, boolean, ...string[]]
+          // -> last(this) extends last(other)
+          // -> first(other) extends first(this)
+          // -> [boolean, string, string] extends [boolean, ...string[]] (remove common first)
+          // -> [...number[], boolean] extends [number, number, boolean] (remove common last)
+
+
+
+          // -> [...number[]] extends [number, number, boolean]
+          // -> [boolean, string, string] extends [...string[]]
+          // -> [b] extends [c]
+          let otherUpTilLast = new TupleLiteral(List.of(other.items.slice(0, other.items.length - 1)));
+          if (!this.rest().isSubtypeOf(otherUpTilLast)) {
+            return false;
+          }
+          if ()
+        }
+
+        // check if there are spreads
+
+        if (otherFirst && otherFirst instanceof Spread) {
+          // **NOTE:** this case is already handled by Expr upon
+          // simplification by cg(env, TupleLiteral...)
+          // if (first.ty instanceof TupleLiteral) {
+          //   // check first items match
+          //   for (let i = 0; i < first.ty.items.length; i++) {
+          //     if (!this.items.at(i).isSubtypeOf(first.ty.items.at(i))) {
+          //       return false;
+          //     }
+          //   }
+          // }
+          // [...number[], a, b, c]
+          // first, check last items
+          //   case 1: last item is also a spread
+          //   e.g. [a, ...number[]] extends [...number[], a, b, c]
+
+        } else if (other instanceof ArrayOf) {
+          return this.items.every(item => item.isSubtypeOf(other));
+        } else {
+          return super.isSubtypeOf(other);
+        }
+      }
+    }
+
+    export class StringLiteral extends Node {
+      constructor(public value: string) {
+        super();
+      }
+
+      isSubtypeOf(other: Node): boolean {
+        if (other.equals(Primitive.String())) {
+          return true;
+        } else {
+          return super.isSubtypeOf(other);
+        }
+      }
+    }
+
+    export class BooleanLiteral extends Node {
+      constructor(public value: boolean) {
+        super();
+      }
+
+      isSubtypeOf(other: Node): boolean {
+        if (other.equals(Primitive.Boolean())) {
+          return true;
+        } else {
+          return super.isSubtypeOf(other);
+        }
+      }
+    }
+
+    export class NumberLiteral extends Node {
+      constructor(public value: number) {
+        super();
+      }
+
+      isSubtypeOf(other: Node): boolean {
+        if (other.equals(Primitive.Number())) {
+          return true;
+        } else {
+          return super.isSubtypeOf(other);
+        }
+      }
+    }
--- test.ts
+++ test.ts
@@ -1,15 +1,110 @@
-import {readFileSync} from 'fs';
-import {Parser} from "./src/parser";
+import { readFileSync } from 'fs';
+import { Parser } from "./src/parser";
 import * as AST from './src/ast';
 
 const file = readFileSync('./test.types');
 const tree = Parser.parse(file.toString());
 
-function cg(env: any, ast: AST.Node) {
-	if (ast instanceof AST.CompilationUnit) {
+const builtins: any = {
+  'define': (env: any, sym: AST.Identifier | AST.List<AST.Identifier>, defn: AST.Node): any => {
+    if (sym instanceof AST.List) { // function definition
+      let fnName = sym.first();
+      if (!fnName) {
+        throw new Error(`function name expected but not found in function definition`);
+      }
+      let fnArgs = sym.rest();
+      env[fnName!.text] = (e: any, ...args: AST.Node[]): any => {
+        // make partial function
+        if (args.length < fnArgs.length) {
+          console.log(`making partial of ${fnName!.text} ${args.length} (total ${fnArgs.length})`);
+          return (e: any, ...partialArgs: AST.Node[]) => env[fnName!.text](e, ...args, ...partialArgs);
+        }
 
-	}
+        // eval all args first
+        let locals: any = { ...e };
+        console.log(locals);
+        for (let i = 0; i < args.length; i++) {
+          console.log({ i, args });
+          locals[fnArgs.at(i)!.text] = cg(locals, args[i]);
+        }
+        console.log(locals);
+
+        // run elements in definition
+        return cg(locals, defn);
+      }
+    } else { // plain definition
+      env[sym.text] = defn;
+    }
+  },
+
+  'if': (env: any, predicate: AST.Node, trueCase: AST.Node, falseCase?: AST.Node): any => {
+    let pred = cg(env, predicate) as AST.BooleanLiteral;
+    if (pred.value) {
+      return cg(env, trueCase);
+    }
+
+    if (!falseCase) {
+      return AST.Primitive.Never();
+    } else {
+      return cg(env, falseCase);
+    }
+  },
+
+  'extends?': (env: any, a: AST.Node, b: AST.Node): AST.BooleanLiteral => {
+    let resolvedA = cg(env, a);
+    let resolvedB = cg(env, b);
+    console.log({ resolvedA, resolvedB });
+    return resolvedA.isSubtypeOf(resolvedB);
+  }
+};
+
+function cg(env: any, ast: AST.Node): any {
+  if (ast instanceof AST.Identifier) {
+    let { text } = ast;
+    if (text in builtins) {
+      return builtins[text];
+    } else if (text in env) {
+      return env[text];
+    } else {
+      throw new Error(`unresolved symbol: ${text}`)
+    }
+  } else if (ast instanceof AST.List) {
+    let op = cg(env, ast.first());
+    let args = ast.rest();
+    if (typeof op === 'function') {
+      return op(env, ...args);
+    } else {
+      if (args.length !== 0) {
+        throw new Error(`tried to call ${op} with ${args.length} - not a function`);
+      }
+      return op;
+    }
+  } else if (ast instanceof AST.CompilationUnit) {
+    for (const s of ast.sexprs) {
+      cg(env, s);
+    }
+    return env;
+  } else if (ast instanceof AST.TupleLiteral) {
+    let newItems: AST.SExpr[] = [];
+    for (let i = 0; i < ast.items.length; i++) {
+      // expand spreads that are tuple
+      let item = ast.items.at(i);
+      if (item instanceof AST.Spread && item.ty instanceof AST.TupleLiteral) {
+        newItems = [...newItems, ...item.ty.items.map((x: AST.SExpr) => cg(env, x))];
+      } else {
+        newItems = [...newItems, cg(env, item)];
+      }
+    }
+    // handle: [...number[]] -> number[]
+    if (newItems.length === 1 && newItems[0] instanceof AST.Spread) {
+      return newItems[0].ty;
+    }
+    return new AST.TupleLiteral(AST.List.of(newItems));
+  } else {
+    return ast;
+  }
 }
 
-console.log(JSON.stringify(tree.toData(), null, 2));
-
+let env = {};
+let { Lens } = cg(env, tree);
+console.log(env);
